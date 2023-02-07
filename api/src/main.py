96e540585b1f2a911b73f1447c2301bf48b44def
Tom Genlis <bbryckout@gmail.com>
2023-02-07 15:37:37 +0100

    fix(api/code): added comments and simplified code structure

--- api/src/main.py
+++ api/src/main.py
@@ -2,70 +2,69 @@ from flask import Flask, request
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
-from vit_keras.vit import preprocess_inputs
-import base64
 import json
+from utils import base64_to_tensor, prepro
+import os
 
+api_dir = os.path.join(os.path.dirname(__file__), "..")
+
+# Fetch the dataset from the TensorFlow Datasets Catalog
 ds_both, ds_info = tfds.load("stanford_dogs", as_supervised=False, with_info=True)
 
-classes = ds_info.features['label'].names
-classes = ['-'.join(c.split('-')[1:]) for c in classes]
+# Get the class names from the dataset info
+classes = ds_info.features["label"].names
 
+# Remove the "n*" prefix from the class names
+classes = ["-".join(c.split("-")[1:]) for c in classes]
+
+# Number of classes
 n_classes = len(classes)
 
-ds_train, ds_test = ds_both["train"], ds_both["test"]
+# Load the TensorFlow model and compile it for performance
+model = tf.keras.models.load_model(
+    os.path.join(api_dir, "model/model_no_augm.h5"), compile=True
+)
 
-def prepro(img):
-    img = tf.cast(img, dtype=tf.float32)
-    img = preprocess_inputs(img)
-    img = tf.image.resize(img, [256, 256])
-    
-    return img
-
-app = Flask("Dog Vision")
-
-# Load your TensorFlow model
-model = tf.keras.models.load_model("model_no_augm.h5", compile=True)
-
-print("Started Web API")
-
-def base64_to_tensor(base64_string):
-  # Decode the base64 string to binary data
-  image_binary = base64.b64decode(base64_string)
-
-  # Convert the binary data to a TensorFlow tensor
-  image_tensor = tf.image.decode_image(image_binary)
-
-  return image_tensor
+# Create the Flask app
+app = Flask("Woof Vision")
 
 
+# Define the route for the prediction
 @app.route("/predict", methods=["POST"])
 def predict():
-    # Get the image from the GET request
+    """Make a prediction with the model"""
+
+    # Get the image in base64 from the JSON of the POST request
     data = request.json["image"]
+
+    # Convert the base64 string to a TensorFlow tensor (image)
     image = base64_to_tensor(data)
-    
+
     # Preprocess the image
     image = prepro(image)
+
+    # Add a batch dimension
     image = np.reshape(image, [1, *image.shape])
-    
+
     # Make a prediction with the model
     prediction = model.predict(image)[0]
 
+    # Get the 3 best matches my matching the prediction with the classes
     hashmap = {}
     for i, key in enumerate(classes):
         hashmap[key] = prediction[i]
-    matches = dict(sorted(hashmap.items(), key=lambda x:-x[1])[:3]) # 3 best matches
-    
-    res = { "prediction": matches }
-    
+    matches = dict(sorted(hashmap.items(), key=lambda x: -x[1])[:3])
+
+    body = {"prediction": matches}
+
+    # Return the prediction as a JSON
     response = app.response_class(
-        response=json.dumps(str(res)),
-        status=200,
-        mimetype='application/json'
+        response=json.dumps(str(body)), status=200, mimetype="application/json"
     )
 
     return response
 
+
+# Run the app
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ api/src/utils.py
@@ -0,0 +1,23 @@
+from vit_keras import vit
+import base64
+import tensorflow as tf
+
+
+def prepro(img):
+    """Preprocess the image to be compatible with the model"""
+    img = tf.cast(img, dtype=tf.float32)
+    img = vit.preprocess_inputs(img)
+    img = tf.image.resize(img, [256, 256])
+
+    return img
+
+
+def base64_to_tensor(base64_string):
+    """Convert a base64 string to a TensorFlow tensor image"""
+    # Decode the base64 string to binary data
+    image_binary = base64.b64decode(base64_string)
+
+    # Convert the binary data to a TensorFlow tensor
+    image_tensor = tf.image.decode_image(image_binary)
+
+    return image_tensor
